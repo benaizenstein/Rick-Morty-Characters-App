54b0e60743917e82634e231f812962da4d6a1838
benaizenstein <52672943+benaizenstein@users.noreply.github.com>
2024-01-23 22:17:59 +0200

    created config file

---
+++ rick-and-morty/src/config/metadata.ts
@@ -0,0 +1,6 @@
+export const metadata = {
+    filters: [
+        { label: 'Gender', type: 'gender', options: ['Male', 'Female', 'Genderless', 'Unknown'] },
+        { label: 'Status', type: 'status', options: ['Alive', 'Dead', 'Unknown'] },
+    ]
+}
