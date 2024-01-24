9bf896b782b8fc4926864abd07b0d911236ae74b
benaizenstein <52672943+benaizenstein@users.noreply.github.com>
2024-01-24 12:14:16 +0200

    Add debounce method

--- rick-and-morty/src/components/Search/Search.tsx
+++ rick-and-morty/src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextField } from '@mui/material';
 import ISearch from '../../interfaces/Search';
 import { metadata } from '../../config/metadata';
@@ -7,10 +7,17 @@ import { metadata } from '../../config/metadata';
 const Search: React.FC<ISearch> = ({ onSearchChange }) => {
     const [searchQuery, setSearchQuery] = useState<string>('');
 
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            onSearchChange(searchQuery);
+        }, 300);
+
+        return () => clearTimeout(delayDebounceFn);
+    }, [searchQuery, onSearchChange]);
+
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
         setSearchQuery(value);
-        onSearchChange(value);
     };
 
     return (
